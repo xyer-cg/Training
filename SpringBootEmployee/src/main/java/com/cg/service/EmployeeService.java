5e42fa099b372907bdf0117ad98ee76c269078ce
xyer-cg <xuan-yu.er@capgemini.com>
2018-11-29 15:21:43 +0800

    hello

--- SpringBootEmployee/src/main/java/com/cg/service/EmployeeService.java
+++ SpringBootEmployee/src/main/java/com/cg/service/EmployeeService.java
@@ -1,21 +1,44 @@
 package com.cg.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import com.cg.beans.Employee;
 import com.cg.repository.EmployeeRepository;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @Service
 public class EmployeeService
 {
 	@Autowired
 	private EmployeeRepository employeeRepository;
+	
+	@PostConstruct
+	public void init ()
+	{
+		try
+		{
+			BufferedReader reader = new BufferedReader (new InputStreamReader (new ClassPathResource ("employees.json").getInputStream ()));
+			List <Employee> employees = new Gson ().fromJson (reader, new TypeToken <List <Employee>> () {}.getType ());
+			employees.stream ().forEach (employee -> addEmployee (employee));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 	
 	public List <Employee> getAllEmployees ()
 	{
